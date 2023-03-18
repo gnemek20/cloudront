dfdab00064a0c773e4246908d677dc799adb73d2
gnemek20 <gnemek20@gmail.com>
2023-03-19 00:31:51 +0900

    axios 설치 및 기본 설정

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,14 @@ module.exports = defineConfig({
   devServer: {
     allowedHosts: "all",
     port: 80,
+    proxy: {
+      "/api": {
+        target: "https://port-0-cloudack-6g2llfe1pmto.sel3.cloudtype.app/",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": '',
+        }
+      }
+    }
   }
 })
