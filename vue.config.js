2cf3ab750f1d191e62533d50acb803f9b5323531
gnemek20 <gnemek20@gmail.com>
2023-03-19 00:58:16 +0900

    코드 스타일 변경

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
         target: "https://port-0-cloudack-6g2llfe1pmto.sel3.cloudtype.app/",
         changeOrigin: true,
         pathRewrite: {
-          "^/api": '',
+          "^/api": ''
         }
       }
     }
