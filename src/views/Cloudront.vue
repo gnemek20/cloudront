503ac0de47440a6a1d2df726b261015ead3248a0
gnemek20 <gnemek20@gmail.com>
2023-03-19 00:41:11 +0900

    axios 확인

--- src/views/Cloudront.vue
+++ src/views/Cloudront.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
     <h1>안녕하세요!</h1>
+    <p>{{ cwd }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cwd: '',
+    }
+  },
+  created() {
+    this.$api.get('/api/cwd').then((res) => {
+      if (res.status === 200) {
+        this.cwd = res.data;
+      }
+      else {
+        this.cwd = "axios 에러";
+      }
+    })
+  }
+}
+</script>
