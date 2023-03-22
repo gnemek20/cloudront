affd9e4d8ea25f509bb0169d160333623819b7e6
gnemek20 <gnemek20@gmail.com>
2023-03-23 02:23:43 +0900

    axios 주소 로컬에서 서버로 변경

--- src/global/functions.js
+++ src/global/functions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const address = 'https://port-0-cloudack-6g2llfe1pmto.sel3.cloudtype.app'
-const address = 'http://waat.kro.kr:3000'
+const address = 'https://port-0-cloudack-6g2llfe1pmto.sel3.cloudtype.app'
+// const address = 'http://waat.kro.kr:3000'
 
 export default {
   install(Vue) {
